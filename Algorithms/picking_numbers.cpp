749fdcb4d310396b3e780dedf2c69821c379d719
coderavana <coderavana@gmail.com>
2017-04-09 14:46:12 +0530

    started HackerEarth

---
+++ Algorithms/picking_numbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+    int n,a[100],b[100],num;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>num;
+        b[num]++;
+    }
+}
